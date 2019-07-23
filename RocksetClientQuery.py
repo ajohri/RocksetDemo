13732a949f10c34d9101ac65cc2d5dc21cf1a098
Abhishek Johri <johri3@illinois.edu>
2019-07-22 20:17:46 -0700

    Updated readme file

--- RocksetClientQuery.py
+++ RocksetClientQuery.py
@@ -1,5 +1,7 @@
 from rockset import Client, Q
 
+# TODO create config files to store api key
+# Use the config method in the Rockset client
 rs = Client(api_key='9s2DcuS7Ola8Ykt9ZreZ5lJHTm9gYgmt8kVbN8LobnCzK5eqsMtSPIw8xxTh4cal', api_server='https://api.rs2.usw2.rockset.com')
 
 results = rs.sql(Q('''SELECT c."age" FROM "commons"."Testing2" c'''))
